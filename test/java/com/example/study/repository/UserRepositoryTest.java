6d791cca9090e0febb734600cfbd37c1bd5d51c2
syd9538 <syd9538@gmail.com>
2020-03-12 22:29:03 +0900

    CategoryRepository, UserRepository Create, Read 테스트 코드 추가

--- test/java/com/example/study/repository/UserRepositoryTest.java
+++ test/java/com/example/study/repository/UserRepositoryTest.java
@@ -24,31 +24,36 @@ public class UserRepositoryTest extends StudyApplicationTests {
     @Test
 
     public void create(){
+        String account = "Test01";
+        String password = "Test01";
+        String status = "REGISTERED";
+        String email = "Test01@gmail.com";
+        String phoneNumber = "010-1111-2222";
+        LocalDateTime registeredAt = LocalDateTime.now();
+        LocalDateTime createdAt = LocalDateTime.now();
+        String createdBy = "AdminServer";
 
-        //String sql = insert in to user (%s, %s, %d) value (account, email, age);
         User user = new User();
-        user.setAccount("TestUser03");
-        user.setEmail("TestUser03@gmail.com");
-        user.setPhoneNumber("010-1111-3333");
-        user.setCreatedAt(LocalDateTime.now());
-        user.setCreatedBy("TestUser2");
+        user.setAccount(account);
+        user.setPassword(password);
+        user.setStatus(status);
+        user.setEmail(email);
+        user.setPhoneNumber(phoneNumber);
+        user.setRegisteredAt(registeredAt);
+        user.setCreatedAt(createdAt);
+        user.setCreatedBy(createdBy);
 
         User newUser = userRepository.save(user);
-        System.out.println("newUser : " + newUser);
+        Assertions.assertNotNull(newUser);
+
 
     }
     @Test
     @Transactional
     public void read(){
-        Optional<User> user = userRepository.findByAccount("TestUser03");
+        User user = userRepository.findFirstByPhoneNumberOrderByIdDesc("010-1111-2222");
 
-        user.ifPresent(selectUser ->{
-          selectUser.getOrderDetailList().stream().forEach(detail ->{
-//              System.out.println(detail.getItemId());
-              Item item = detail.getItem();
-              System.out.println(item);
-          });
-        });
+        Assertions.assertNotNull(user);
     }
     @Test
     public void update(){
