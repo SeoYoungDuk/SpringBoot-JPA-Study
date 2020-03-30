4578aa1598d1498d4d6e87c6422133a2cfb01bf0
syd9538 <syd9538@gmail.com>
2020-03-30 22:17:58 +0900

    상품 Rest API Create

---
+++ main/java/com/example/study/model/network/response/ItemApiResponse.java
@@ -0,0 +1,36 @@
+package com.example.study.model.network.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+//따로만들어주는 이유는 그때 그때 상황마다 달라질수 있어서 나중에 수정할때 편리해짐 !
+public class ItemApiResponse {
+    private Long id;
+
+    private String status;
+
+    private String name;
+
+    private String title;
+
+    private String content;
+
+    private BigDecimal price;
+
+    private String brandName;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+
+    private Long partnerId;
+}
