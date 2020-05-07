a86c7fc2ce5c339b1982653858ee5721fe865796
syd9538 <syd9538@gmail.com>
2020-05-07 22:53:34 +0900

    Item,OrderGroup JPA Enum 泥섎━

--- main/java/com/example/study/service/ItemApiLogicService.java
+++ main/java/com/example/study/service/ItemApiLogicService.java
@@ -82,6 +82,9 @@ public class ItemApiLogicService implements CrudInterface<ItemApiRequest, ItemAp
     }
 
     private  Header<ItemApiResponse> response(Item item){
+
+//        String statusTitle = item.getStatus().getTitle();
+
         ItemApiResponse body = ItemApiResponse.builder()
                 .id(item.getId())
                 .status(item.getStatus())
